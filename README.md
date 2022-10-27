2f51e2502c2bc6f70a76d6622010f6aa4673b2e8
bloodimooni <joshua@infiniment.de>
2022-10-27 04:54:12 +0200

    Finished version 2 and made huge changes

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # wan-ranger
 This tool was build to make IP-host discovery faster
-it is using multiprocessing to create multiple threads to check all 254 Hosts of a C-Type Network at once.
+it is using multiprocessing to create multiple threads to check all 254 Hosts of any /24 Network at once. It is also capable of checking a /16 Network for all available hosts.
 
 # how to use
-python3 wanranger.py (IP range -> example: 10.0.1.) (Save to file? yes/no)
+
+python3 wanranger.py VAR1 VAR2
+
+VAR1 --> The IP-Range you want to scan (for /24 -> 192.168.1.0 | for /16 ->192.168.0.0)
+VAR2 --> yes/no for saving the output of the scans to a file in the same directory.
+
+-- The Program currently sees a 0 as HOSTS so if you would want to scan the Network 192.168.0.0/24 You just need to CTRL + C the scan after its done with the first one. 
 
 # requirements
 
--- wanranger.py
+++ wanranger.py
@@ -11,15 +11,16 @@ except:
 
 
 class IP:
-    def __init__(self, front, save_file):
-        self.front = front
+    def __init__(self, ip_address, save_file):
+        self.ip_address = ip_address
+        self.host_address = self.ip_address.split(".")
         self.reachable = []
         self.unreachable = []
         self.save_file = save_file
 
     def ping(self,host):
-        ip = self.front + str(host)
-        ping_reply = subprocess.run(["ping","-c","1", ip],stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        host_ip = "%s.%s.%s.%s" % (self.host_address[0],self.host_address[1],self.host_address[2],str(int(self.host_address[3])+host))
+        ping_reply = subprocess.run(["ping","-c","1", "%s" % host_ip],stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
         if ping_reply.returncode == 0:
             if ("unreachable" in str(ping_reply.stdout)):
@@ -31,43 +32,65 @@ class IP:
             self.unreachable.append(host)
 
     def save(self):
-        with open("results_for_%s.txt" % self.front,"w+") as res:
-            res.write("\t## Scan for %s ##\n\n" % self.front)
-            if self.save_file == True:
+        current_ip = "%s.%s.%s.%s" % (self.host_address[0],self.host_address[1],self.host_address[2],"0")
+        if self.save_file == True and len(self.reachable) > 0:
+            with open("results_for_%s.txt" % current_ip ,"w+") as res:
+                res.write("\t## Scan for %s ##\n\n" % self.ip_address)
                 res.write("\t ++ alive hosts ++\n")
                 for host in self.reachable:
-                    res.write("Host - %s%s is reachable\n" % (self.front,host))
-                res.write("\n\t -- down hosts --\n")
-                for host in self.unreachable:
-                    res.write("Host - %s%s is unreachable\n" % (self.front,host))
+                    res.write("Host - %s.%s.%s.%s is reachable\n" % (self.host_address[0],self.host_address[1],self.host_address[2],host))
+
+
+    def figure_out_type(self):
+        if self.host_address[2] == "0" and self.host_address[3] == "0":
+            #cp("We have a B class network here.","blue")
+            return 16
+
+        elif self.host_address[2] != "0" and self.host_address[3] == "0":
+            #cp("We have a C class network here.","blue")
+            return 24
+
 
 def clear():
     print("\n"*100)
 
-Banner = "\n\n\t*****************\n" + \
-             "\t* WAN-Ranger by *\n" + \
-             "\t*  BloodiMooni  *\n" + \
-             "\t*****************\n\n" + \
-             "\n\tCTLR + C to exit\n\n"
 
-while True:
+def scan_16(ip,ip_address):
+    for I in range(0,256):
+        ip.host_address[2] = str(I)
+        try:
+            cp("[~] Started scan for %s.%s.%s.%s/16" % (ip.host_address[0],ip.host_address[1],ip.host_address[2],"0"),"green")
+            pool = mp.Pool(254)
+            pool.map(ip.ping,range(1,255))
+            pool.close()
+            pool.join()
+            ip.reachable.sort()
+            ip.unreachable.sort()
+
+            if not len(ip.reachable) == 0:
+                cp("\n\t++ Alive Hosts ++","green")
+                for host in ip.reachable:
+                    cp("[+] Host : %s.%s.%s.%s is up" % (ip.host_address[0],ip.host_address[1],ip.host_address[2],host), "green")
+
+            else:
+                cp("\n\t-- No alive hosts found --", "red")
+                #for host in ip.unreachable:
+                    #cp("[-] Host : %s.%s.%s.%s is down" % (ip.host_address[0],ip.host_address[1],ip.host_address[2],host), "red")
+
+            cp("\n ************ SCAN COMPLETE ************\n\n", "yellow")
+            ip.save()
+            ip.reachable = []
+            ip.unreachable = []
+
+
+        except KeyboardInterrupt:
+            cp("\n\n[~] USER INTERRUPT - Exiting.","red")
+            sys.exit(0)
+
+
+def scan_24(ip,ip_address):
     try:
-        clear()
-        cp(Banner, "yellow")
-        front = sys.argv[1]
-        save_file = sys.argv[2]
-    except:
-        cp("Commandline arguments gave an error.","red")
-        sys.exit(1)
-
-    try:
-        if "y" in save_file.lower():
-            save_file = True
-        else:
-            save_file = False
-
-        ip = IP(front,save_file)
-        cp("[~] Started scan for %s" % ip.front,"green")
+        cp("[~] Started scan for %s/24" % ip.ip_address,"green")
         pool = mp.Pool(254)
         pool.map(ip.ping,range(1,255))
         pool.close()
@@ -77,16 +100,44 @@ while True:
 
         cp("\n\t++ reachable ++","green")
         for host in ip.reachable:
-            cp("[+] Host : %s%s is up" % (ip.front,host), "green")
+            cp("[+] Host : %s.%s.%s.%s is up" % (ip.host_address[0],ip.host_address[1],ip.host_address[2],host), "green")
 
         cp("\n\t-- unreachable --", "red")
         for host in ip.unreachable:
-            cp("[-] Host : %s%s is down" % (ip.front,host), "red")
+            cp("[-] Host : %s.%s.%s.%s is down" % (ip.host_address[0],ip.host_address[1],ip.host_address[2],host), "red")
 
         cp("\n ************ SCAN COMPLETE ************\n\n", "yellow")
         ip.save()
-        break
+        return True
 
     except KeyboardInterrupt:
         cp("\n\n[~] USER INTERRUPT - Exiting.","red")
-        break
+        sys.exit(0)
+
+
+Banner = "\n\n\t*****************\n" + \
+             "\t* WAN-Ranger by *\n" + \
+             "\t*  BloodiMooni  *\n" + \
+             "\t*****************\n\n" + \
+             "\n\tCTLR + C to exit\n\n"
+
+
+#try:
+clear()
+cp(Banner, "yellow")
+ip_address = sys.argv[1]
+save_file = sys.argv[2]
+
+ip = IP(ip_address,save_file)
+if "y" in save_file.lower():
+    ip.save_file = True
+else:
+    ip.save_file = False
+if ip.figure_out_type() == 24:
+    scan_24(ip,ip_address)
+elif ip.figure_out_type() == 16:
+    scan_16(ip,ip_address)
+
+#except:
+#    cp("Commandline arguments gave an error.","red")
+#    sys.exit(1)
